adbaa8e5602b44cb1e04088046d3020aeae743d5
^_^ <^_^@arbitrarysystems.com>
2019-02-28 15:31:12 +0100

    updated textures system

--- update-textures.js
+++ update-textures.js
@@ -1,19 +1,43 @@
 const PSD = require('./node_modules/psd.js/');
 
-const psd = PSD.fromFile('./src/assets/img/textures.psd');
-psd.parse()
 
-var i,
-	layer,
-	dir = './src/assets/img/',
-	file;
-for(i in psd.layers){
-	layer = psd.layers[i];
-	if( layer.name.indexOf('_') === 0 ){
-		// EXPORT
-		file = layer.name + '.png';
-		layer.image.saveAsPng(dir + file).then( function(){
-			console.log('exported:', dir + file );
-		});
-	}	
+const srcDir = './src/assets/psd/';
+const targetDir = './src/assets/img/textures/';
+const groups = ['surface', 'road', 'build'];
+
+let json = {};
+
+for(var i=0;i<groups.length;i++){
+	
+	var psd = PSD.fromFile(srcDir + groups[i] + '.psd')
+	psd.parse();
+
+	// CREATE JSON NODE
+	json[ groups[i] ] = {};
+
+	// EXPORT LAYERS
+	var j, layer;
+	for(j in psd.layers){
+		layer = psd.layers[j];
+		if( layer.name.indexOf('_') === 0 ){
+			// EXPORT
+			var name = groups[i] + layer.name + '.png';
+			var callback = ( function(n){ return ()=>{ console.log(n) } })(targetDir + name + '... ready')
+			layer.image.saveAsPng( targetDir + name ).then( callback );
+
+			// INSERT URL
+			json[ groups[i] ][layer.name] = targetDir + name;
+
+		}	
+	}
+
+
 }
+
+// WRITE JSON DATA
+var fs = require('fs');
+fs.writeFile( targetDir + 'index.json' , JSON.stringify(json), 'utf8', function(){
+	//console.log('Written JSON:', json)
+});
+
+
