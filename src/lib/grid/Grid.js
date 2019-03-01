e8030071ba45c926e1e9ed7f1bd440354356beed
^_^ <^_^@arbitrarysystems.com>
2019-03-01 17:19:32 +0100

    dragsystem

--- src/lib/grid/Grid.js
+++ src/lib/grid/Grid.js
@@ -16,8 +16,6 @@ const options = (function(){
 	options.tileSkewX = Math.atan2( options.tileWidth, options.tileHeight);
 	options.tileSkewY = Math.atan2( -options.tileHeight, options.tileWidth);
 	
-	options.surfaceSpan = 3;
-
 	return options;
 })();
 
@@ -93,56 +91,103 @@ export class Grid extends PIXI.Container{
 		this.on('pointermove', (e)=>{ this.pointer(e) } );
 		this.on('pointerup',   (e)=>{ this.pointer(e) } );
 		this.on('pointertap',  (e)=>{ this.pointer(e) } );
-		
 	}
+
+	// ADD TILE
+	add(x,y){
+		if( !this.tiles[x] ) this.tiles[x] = {};
+		if( !this.tiles[x][y] ) this.tiles[x][y] = this.addChild( new Tile(x,y) );
+
+		this.size.minx = Math.min(x, this.size.minx );
+		this.size.miny = Math.min(y, this.size.miny );
+		this.size.maxx = Math.max(x, this.size.maxx );
+		this.size.maxy = Math.max(y, this.size.maxy );
+		this.size.width = this.size.maxx - this.size.minx;
+		this.size.height = this.size.maxy - this.size.miny;
+
+		tools.position( tools.transform( this.tiles[x][y] ) );
+		return this.tiles[x][y];
+	}
+	
+	// GET TILE
+	getTile(c,isCoordinate=true){
+		if( !isCoordinate ) c = tools.p2c(c.x-this.x,c.y-this.y);
+		if( !this.tiles[c.x] ) return undefined;
+		return this.tiles[c.x][c.y];
+	}
+
+	// GET TILE ARRAY
+	getTileArray(tile, width = 1, height = width){
+		
+		if( !tile ) return [];
+		
+		let cx = Math.min( Math.max( tile.cx - Math.ceil(width*0.5)  + 1, this.size.minx ), this.size.maxx - width  + 1),
+			cy = Math.min( Math.max( tile.cy - Math.ceil(height*0.5) + 1, this.size.miny ), this.size.maxy - height + 1),
+			a = [],x,y;
+		for(x=cx;x<cx+width;x++){
+			for(y=cy;y<cy+height;y++){
+				a.push( this.getTile({x:x,y:y}, true) );
+			}
+		}
+		return a;
+	}
+
+
 	get mode(){
 		return App.Interface.mode();
 	}
+
 	pointer( e){
 
 		if( e.type === 'pointerdown'){
-			
-			if( this.mode === 'drag' ){
-				this._dragStart = {x:e.data.global.x, y:e.data.global.y};
-			}else if( this.mode === 'road' ){
-				this._pathStart = this.getTileFromEvent(e);
-			}
-
+			this.__pd = true;
+			this.__ps = {x:e.data.global.x, y:e.data.global.y};
+			this.__pp = {x:e.data.global.x, y:e.data.global.y};
 		}else if( e.type === 'pointermove'){
+			
+			this.__pc = {x:e.data.global.x, y:e.data.global.y};
+			
 
-			if( this._dragStart ){
-				this.x -= this._dragStart.x - e.data.global.x;
-				this.y -= this._dragStart.y - e.data.global.y;
-				this._dragStart = {x:e.data.global.x, y:e.data.global.y};
+			if( this.mode === 'drag' && this.__pd ){
+				this.drag(this.__pc.x - this.__pp.x, this.__pc.y - this.__pp.y);
+			}else if( this.mode === 'road' && this.__pd ){
+				this.path( this.getTile(this.__ps,false), this.getTile(this.__pc,false) );
+			}else{
+				this.hover();
 			}
 
-			let tile = this.getTileFromEvent(e);
-			if( tile !== this._previousHoverTile ){
 
-				if( this._previousHoverTile ) this._previousHoverTile.hover = false;
-				if( tile ){
-					this._previousHoverTile = tile;
-					tile.hover = true;
-				}
 
-				if( this.mode === 'road' && this._pathStart ){
-					this.path(this._pathStart, tile);
-				}
-			}
+			this.__pp = {x:e.data.global.x, y:e.data.global.y};
+
 
 		}else if( e.type ===  'pointerup'){
 			
-			this._dragStart = false;
-			this._pathStart = false;
+			delete this.__ps;
+			delete this.__pc;
+			delete this.__pp;
+			this.__pd = false;
 		
 		}else if( e.type ===  'pointertap'){
 			
-			if( this.mode === 'drag' ){
+			/*if( this.mode === 'drag' ){
 				this.selected = this.getTileFromEvent(e);
-			}
+			}*/
 
 		}
 	}
+
+	drag(dx, dy){
+		this.x += dx;
+		this.y += dy;
+	}
+
+	hover(width = 1, height = width){
+		if( this._hover ) this._hover.forEach( (tile)=>{ if(tile) tile.hover = false;} );
+		this._hover = this.getTileArray( this.getTile(this.__pc, false) ,width ,height );
+		this._hover.forEach( (tile)=>{ if(tile) tile.hover = true;} );
+	}
+
 	set selected(array){
 
 		// DESELECT ALL
@@ -159,24 +204,8 @@ export class Grid extends PIXI.Container{
 			for(var i=0;i<this._selected.length;i++) this._selected[i].selected = true;
 		}
 	}
-	add(x,y){
-		if( !this.tiles[x] ) this.tiles[x] = {};
-		if( !this.tiles[x][y] ) this.tiles[x][y] = this.addChild( new Tile(x,y) );
+	
 
-		this.size.minx = Math.min(x, this.size.minx );
-		this.size.miny = Math.min(y, this.size.miny );
-		this.size.maxx = Math.max(x, this.size.maxx );
-		this.size.maxy = Math.max(y, this.size.maxy );
-		this.size.width = this.size.maxx - this.size.minx;
-		this.size.height = this.size.maxy - this.size.miny;
-
-		tools.position( tools.transform( this.tiles[x][y] ) );
-		return this.tiles[x][y];
-	}
-	get(x,y){
-		if( !this.tiles[x] ) return undefined;
-		return this.tiles[x][y];
-	}
 	path(from, to){
 		
 		// CREATE A PATHFINDER GRID
@@ -196,18 +225,14 @@ export class Grid extends PIXI.Container{
 		// CONVERT RESULTS TO TILE-ARRAY
 		let i, n=result.length;
 		for(i=0;i<n;i++){
-			result[i] = this.get(result[i][0]+this.size.minx, result[i][1]+this.size.miny);
+			result[i] = this.getTile( {x:result[i][0]+this.size.minx, y:result[i][1]+this.size.miny} );
 		}
 
 		// SET SELECTION
 		this.selected = result;
 
 	}
-	getTileFromEvent(e){
-		let p = {x:e.data.global.x - this.x, y:e.data.global.y - this.y},
-			c = tools.p2c(p.x,p.y);
-		return this.get(c.x, c.y);
-	}
+	
 }
 
 
